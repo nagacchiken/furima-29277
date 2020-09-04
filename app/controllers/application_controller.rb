53aa42e703045f8eb15dd653a25b1baa5dfb18d3
nagacchiken <vx458pl4@gmail.com>
2020-09-04 19:12:36 +0900

    configure_permitted_parametersの設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  protected
 
-  def index
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:family_name,:first_name,:family_name_kana,:first_name_kana,:birthday])
   end
 
   private
