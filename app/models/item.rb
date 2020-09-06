275db7c39ec6498eae8f9ffa0bf55e45292eb9ef
nagacchiken <vx458pl4@gmail.com>
2020-09-06 21:32:17 +0900

    active_hash用のモデル作成並びにアソシエーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,7 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :date_of_shipment
+  belongs_to_active_hash :delivery_fee
+  belongs_to_active_hash :item_status
 end
