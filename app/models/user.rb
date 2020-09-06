95884b1c8ed11dece7ef9b1c3c469d0d4db45510
nagacchiken <vx458pl4@gmail.com>
2020-09-06 16:24:27 +0900

    単体テストの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
-  validates :nickname, :family_name,:first_name,:family_name_kana,:first_name_kana,:birthday, presence: true
+
+  validates :nickname, :family_name, :first_name, :family_name_kana, :first_name_kana, :birthday, presence: true
   validates :email, uniqueness: true
-  validates :email, inclusion: { in: ["@"]}
-  validates :password, length:{minimum: 6}
+  validates :email, exclusion: { in: ['@'] }
+  validates :password, length: {minimum: 6}
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
   validates :password, confirmation: true
   validates :password_confirmation, presence: true
-  validates :family_name,:first_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
-  validates :family_name_kana,:first_name_kana,format: { with: /\A[ァ-ン]+\z/ }
+  validates :family_name, :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+  validates :family_name_kana, :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
 end
