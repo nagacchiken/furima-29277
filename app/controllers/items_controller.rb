cdc38f8a69afd472ed10bf033ea35c29ba1b8820
nagacchiken <vx458pl4@gmail.com>
2020-09-07 17:50:49 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,4 +5,19 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image,:name,:description,:category_id,:price,:item_status_id,:delivery_fee_id,:date_of_shipment_id, :shipping_region_id).merge(user_id: current_user.id)
+  end
 end
