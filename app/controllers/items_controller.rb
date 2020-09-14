0a35db1d22882bfac80fcc2396b03c5e87d6ab4f
nagacchiken <vx458pl4@gmail.com>
2020-09-14 14:17:36 +0900

    クレジット決済機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -50,7 +50,10 @@ class ItemsController < ApplicationController
 
   def transactions_create
     @purchase = UserPurchase.new(purchase_params)
+
     if @purchase.valid?
+      
+      pay_item
       @purchase.save
       return redirect_to root_path
     else
@@ -74,6 +77,16 @@ class ItemsController < ApplicationController
 
   def purchase_params
     @item = Item.find(params[:id])
-    params.permit(:postal_code, :prefecture_id, :city, :address, :building_name, :phone_number).merge(item_id:@item.id, user_id: current_user.id)
+    params.permit(:token,:postal_code, :prefecture_id, :city, :address, :building_name, :phone_number).merge(price:@item.price,item_id:@item.id, user_id: current_user.id)
+  end
+
+  def pay_item
+    
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: purchase_params[:price], 
+      card: purchase_params[:token], 
+      currency:'jpy' 
+    )
   end
 end
--- app/forms/user_purchase.rb
+++ app/forms/user_purchase.rb
@@ -1,7 +1,7 @@
 class UserPurchase
 
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :address, :building_name, :phone_number, :item_id, :user_id, :purchase_id
+  attr_accessor :postal_code, :prefecture_id, :city, :address, :building_name, :phone_number, :item_id, :user_id, :purchase_id, :token, :price
 
   
 
