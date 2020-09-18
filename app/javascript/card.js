ee0b70e027582d127bd427b6a4a51b0a80f16846
nagacchiken <vx458pl4@gmail.com>
2020-09-18 11:05:53 +0900

    console.logの確認

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -13,7 +13,9 @@ const pay = () => {
       exp_month: formData.get("user_purchase[exp_month]"),
       exp_year: `20${formData.get("user_purchase[exp_year]")}`,
     };
+    console.log(card)
     Payjp.createToken(card, (status, response) => {
+      console.log(response.error)
       if (status == 200) {
         const token = response.id;
         const renderDom = document.getElementById("charge-form");
